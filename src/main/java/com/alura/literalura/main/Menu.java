71ea09df19de44f1169544d08555e0cb329024e3
elgaricito <edgarjaguirre@hotmail.com>
2024-11-17 08:10:18 -0300

    Creando la clase menu en donde el usuario interactua

---
+++ src/main/java/com/alura/literalura/main/Menu.java
@@ -0,0 +1,68 @@
+package com.alura.literalura.main;
+
+import com.alura.literalura.service.ConsumoAPI;
+import com.alura.literalura.service.ConvierteDatos;
+
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner teclado=new Scanner(System.in);
+    private ConsumoAPI consumoAPI =new ConsumoAPI();
+    private final String URL_BASE="https://gutendex.com";
+    private ConvierteDatos conversor=new ConvierteDatos();
+
+
+    public void muestraElMenu() {
+        var opcion = -1;
+        while (opcion != 0) {
+            var menu = """
+                    1 - Buscar libro por titulo 
+                    2 - Buscar libros registrados
+                    3 - Listar autores registrados
+                    4 - Buscar autores vivos en un determinado año
+                    5 - Listar libros por idioma
+                    6 - Salir
+                                  
+                    0 - Salir
+                    """;
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion) {
+                case 1:
+//                    buscarSerieWeb();
+                    break;
+                case 2:
+//                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+//                    mostrarSeriesBuscadas();
+                    break;
+                case 4:
+//                    buscarSeriesPorTitulo();
+                    break;
+                case 5:
+//                    buscarTop5Series();
+                    break;
+                case 6:
+//                    buscarseriesPorCategoria();
+                    break;
+                case 7:
+//                    filtrarSeriePorTemporadaYEvaluaion();
+                case 8:
+//                    buscarEpisodiosPorTitulo();
+                    break;
+                case 9:
+//                    buscarTop5Episodios();
+                    break;
+                case 0:
+                    System.out.println("Cerrando la aplicación...");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+
+    }
+}
