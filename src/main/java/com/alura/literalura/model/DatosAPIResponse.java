a67442e1a09c6547746e65b23104e3f5c89f256e
elgaricito <edgarjaguirre@hotmail.com>
2024-11-17 09:57:49 -0300

    Dado que la respuesta tiene objetos adentro (libros encontrados)debemos crear una clase que tenga las respuestas directas de la api y de ahi recien mapeamos el resto. Hago solo un record, ya que esta clase no va a la base de datos

---
+++ src/main/java/com/alura/literalura/model/DatosAPIResponse.java
@@ -0,0 +1,15 @@
+package com.alura.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAPIResponse(
+        @JsonAlias("count") int count,
+        @JsonAlias("next") String next,
+        @JsonAlias("previous") String previous,
+        @JsonAlias("results")List<DatosLibro> results
+        ) {
+}
