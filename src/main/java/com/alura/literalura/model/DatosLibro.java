da0a8b86a884af02bf3f516faf604c6f91e6f014
elgaricito <edgarjaguirre@hotmail.com>
2024-11-30 10:49:58 -0300

    Cambio el nombre del identificador que entrega la API y el tipo a Long

--- src/main/java/com/alura/literalura/model/DatosLibro.java
+++ src/main/java/com/alura/literalura/model/DatosLibro.java
@@ -2,12 +2,13 @@ package com.alura.literalura.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.UniqueConstraint;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosLibro(
-        @JsonAlias("id") int apiId,
+        @JsonAlias("id") Long identifyer,
         @JsonAlias("title") String titulo,
         @JsonAlias("authors") List<DatosAutor> autores,
         @JsonAlias("translators") List<DatosTraductor> traductores,
