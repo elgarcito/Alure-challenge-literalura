c8cf848332378dd11d4e82737794849d12752a04
elgaricito <edgarjaguirre@hotmail.com>
2024-11-30 10:53:18 -0300

    Agrego la primera de las consultas a la base de datos para mostrar todos los libros guardados

--- src/main/java/com/alura/literalura/repository/LibroRepository.java
+++ src/main/java/com/alura/literalura/repository/LibroRepository.java
@@ -2,6 +2,10 @@ package com.alura.literalura.repository;
 
 import com.alura.literalura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LibroRepository extends JpaRepository<Libro,Long> {
+    List<Libro> findAll();
 }
